a0089b46ce7f32827ed50fa0e0541e8e9a19f93d
Gaston1980 <gastonfrissiones@yahoo.com.ar>
2022-02-01 16:57:55 -0300

    Homework JSII Totally Done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -91,12 +91,16 @@ function esEntero(numero) {
   
 
 function fizzBuzz(numero) {
-  if(numero)
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
+  return numero;
+}
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-}
+
 
 function operadoresLogicos(num1, num2, num3) {
   if(num1 >num2 && num1>num3 && num1 >0) {
@@ -115,6 +119,16 @@ function operadoresLogicos(num1, num2, num3) {
 
 
 function esPrimo(numero) {
+  if( numero < 2) return false;
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+  
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
@@ -133,21 +147,43 @@ function esVerdadero(valor){
 
 
 function tablaDelSeis(){
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6
+}
 
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
+
   
 }
 
 function tieneTresDigitos(numero){
-
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
-  //Escribe tu código aquí
+
+  if(numero > 99 && numero < 1000){
+    return true
+  }
+  return false
+}
+
+
+  
   
 }
 
-function doWhile(numero) {}
+function doWhile(numero) {
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a;
+}
 
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
